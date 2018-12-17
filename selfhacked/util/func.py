8d625a9187d10fc257c8505f506be2c539fbe095
Michael Kim <mkim0407@gmail.com>
2018-12-17 12:23:16 -0800

    @timed decorator

--- selfhacked/util/func.py
+++ selfhacked/util/func.py
@@ -1,4 +1,7 @@
+import time
+
 from functools import wraps
+from typing import Callable
 
 
 def returns(type):
@@ -8,5 +11,38 @@ def returns(type):
             return type(func(*args, **kwargs))
 
         return __new_func
+
+    return __decor
+
+
+def timed(
+        callback: Callable[[float], None]
+):
+    def __decor(func):
+        @wraps(func)
+        def __new_func(*args, **kwargs):
+
+            start = time.time()
+            try:
+                return func(*args, **kwargs)
+            finally:
+                callback(time.time() - start)
+
+        return __new_func
+
+    return __decor
+
+
+def log_time(
+        log: Callable[[str], None] = print,
+        name=None,
+):
+    def __decor(func):
+        _name = name or func.__name__
+
+        def callback(t):
+            log(f"{_name} call took {t} seconds")
+
+        return timed(callback=callback)(func)
 
     return __decor
--- tests/util/test_func.py
+++ tests/util/test_func.py
@@ -1,4 +1,4 @@
-from selfhacked.util.func import returns
+from selfhacked.util.func import returns, timed, log_time
 
 
 def test_returns():
@@ -10,3 +10,33 @@ def test_returns():
     result = f(5)
     assert isinstance(result, list)
     assert result == [i ** 2 for i in range(5)]
+
+
+def test_timed():
+    logs = []
+
+    @timed(logs.append)
+    def long():
+        for i in range(10000):
+            pass
+
+    long()
+    assert len(logs) == 1
+    assert isinstance(logs[0], float)
+
+
+def test_log_time():
+    logs = []
+
+    @log_time(logs.append)
+    def long():
+        for i in range(10):
+            pass
+
+    long()
+    assert len(logs) == 1
+    out = logs[0].split()
+    assert len(out) == 5
+    assert out[:3] == ['long', 'call', 'took']
+    float(out[3])  # no exception should be raised here
+    assert out[-1] == 'seconds'
