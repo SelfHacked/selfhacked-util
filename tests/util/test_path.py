496e3652aa7e0a4068ff531ad63f151ea07a5685
Michael Kim <mkim0407@gmail.com>
2018-12-19 13:48:01 -0800

    fix test fixture

--- tests/util/test_path.py
+++ tests/util/test_path.py
@@ -3,14 +3,14 @@ import os
 from selfhacked.util.path import mkdir, cd
 
 
-def test_mkdir(tmp_path):
-    path = str(tmp_path)
+def test_mkdir(tmpdir):
+    path = str(tmpdir)
     mkdir(path, 'a', 'b', 'c')
     assert os.path.isdir(os.path.join(path, 'a', 'b', 'c'))
 
 
-def test_cd(tmp_path):
-    os.chdir(str(tmp_path))
+def test_cd(tmpdir):
+    os.chdir(str(tmpdir))
     path = os.path.abspath(os.getcwd())
     target = os.path.join(path, 'a')
     os.mkdir(target)
