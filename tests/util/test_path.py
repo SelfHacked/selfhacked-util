b37ca69410fc33162b4255462620e240e64e1cb0
Michael Kim <mkim0407@gmail.com>
2019-01-18 15:22:29 -0800

    add dependencies in previous tests

--- tests/util/test_path.py
+++ tests/util/test_path.py
@@ -1,14 +1,17 @@
 import os
+import pytest
 
 from selfhacked.util.path import mkdir, cd
 
 
+@pytest.mark.dependency()
 def test_mkdir(tmpdir):
     path = str(tmpdir)
     mkdir(path, 'a', 'b', 'c')
     assert os.path.isdir(os.path.join(path, 'a', 'b', 'c'))
 
 
+@pytest.mark.dependency()
 def test_cd(tmpdir):
     os.chdir(str(tmpdir))
     path = os.path.abspath(os.getcwd())
@@ -20,6 +23,12 @@ def test_cd(tmpdir):
     assert os.getcwd() == path
 
 
+@pytest.mark.dependency(
+    depends=[
+        'test_mkdir',
+        'test_cd',
+    ],
+)
 def test_mkdir_relative(tmpdir):
     with cd(str(tmpdir)):
         mkdir('e')
