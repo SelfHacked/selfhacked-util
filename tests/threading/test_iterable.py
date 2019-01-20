d87f4dad86116abf78bd0b3e1ad60cd29a9e3263
Michael Kim <mkim0407@gmail.com>
2019-01-20 13:43:20 -0800

    threaded iterables

---
+++ tests/threading/test_iterable.py
@@ -0,0 +1,52 @@
+from time import sleep
+
+import pytest
+
+from selfhacked.testing import assert_time
+from selfhacked.threading.iterable import ThreadedPrefetchOneIterator, ThreadedPrefetchAllIterator
+
+
+def dummy_iterable():
+    sleep(0.1)
+    yield 0
+    sleep(0.1)
+    yield 1
+
+
+def _test_prefetch(cls):
+    iterator = cls(dummy_iterable())
+
+    with assert_time(0.1):
+        assert next(iterator) == 0
+
+    sleep(0.1)
+    # the second record should have been prefetched by now
+    with assert_time(0):
+        assert next(iterator) == 1
+
+    with pytest.raises(StopIteration):
+        next(iterator)
+
+
+def test_prefetch_one():
+    _test_prefetch(ThreadedPrefetchOneIterator)
+
+
+def test_prefetch_all():
+    _test_prefetch(ThreadedPrefetchAllIterator)
+
+
+def test_timeout():
+    iterator = ThreadedPrefetchAllIterator(dummy_iterable(), timeout=0)
+    with pytest.raises(iterator.Timeout):
+        next(iterator)
+
+
+def test_wait():
+    iterator = ThreadedPrefetchAllIterator(dummy_iterable(), sleep=0.3)
+    with assert_time(0.3):
+        assert next(iterator) == 0
+
+    # the second record should have been prefetched by now
+    with assert_time(0):
+        assert next(iterator) == 1
