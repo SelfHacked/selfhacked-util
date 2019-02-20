d4389c91ed322be6bbe480ff34a177006e69ebae
Michael Kim <mkim0407@gmail.com>
2019-02-19 17:32:35 -0800

    s3 stream

--- setup.py
+++ setup.py
@@ -3,11 +3,18 @@ from setuptools import setup, find_packages
 extra_django = [
     'Django>=1.11',
 ]
+extra_aws = [
+    'boto3>=1.9',
+    'botocore',
+]
+extra_all = extra_django + extra_aws
+
 extra_test = [
     'pytest>=4',
     'pytest-runner>=4',
     'pytest-dependency @ https://github.com/SelfHacked/pytest-dependency/archive/master.zip',
 ]
+extra_dev = extra_all + extra_test
 
 setup(
     name='selfhacked-util',
@@ -18,7 +25,12 @@ setup(
 
     extras_require={
         'django': extra_django,
+        'aws': extra_aws,
+
+        'all': extra_all,
+
         'test': extra_test,
+        'dev': extra_dev,
     },
 
     packages=find_packages(),
