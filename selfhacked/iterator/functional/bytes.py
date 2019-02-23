44bab3c4553cb61ad7f21dd8aa8d2c94f3632e6d
Michael Kim <mkim0407@gmail.com>
2019-02-22 17:50:39 -0800

    use `with` context for gzip file

--- selfhacked/iterator/functional/bytes.py
+++ selfhacked/iterator/functional/bytes.py
@@ -9,4 +9,5 @@ decode = apply(bytes.decode, encoding='utf-8')
 
 def un_gzip(iterable: Iterable[bytes]):
     readable = ReadableIterator(iter(iterable), b'')
-    yield from gzip.open(readable)
+    with gzip.open(readable) as f:
+        yield from f
